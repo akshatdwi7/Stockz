e8123e13ab96ef02105f2c12a4fc73d5da2ab042
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-03-07 12:55:04 +0530

    cleand the folder

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+import { Text, View } from '../../components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text> hey</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    
+  
+  },
+});
---
+++ src/app/modal.tsx
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet } from 'react-native';
+import { Text, View } from '@/components/Themed';
+
+export default function ModalScreen() {
+  return (
+    <View style={styles.container}>
+    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+   
+  },
+});
