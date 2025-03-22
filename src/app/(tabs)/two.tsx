201b1b43e2e97388bed8addbbc2d3acf9c2f0c84
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-03-22 22:50:00 +0530

    Update two.tsx

--- src/app/(tabs)/two.tsx
+++ src/app/(tabs)/two.tsx
@@ -1,11 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, FlatList, ActivityIndicator } from "react-native";
+import { Stack } from "expo-router";
+import { Text, View } from "@/components/Themed";
+import StockListItem from "@/components/StockListItem";
+import { useQuery, gql } from "@apollo/client";
 
-import { Text, View } from '@/components/Themed';
+const query = gql`
+  query MyQuery($user_id: String!) {
+    favoritesByUser_id(user_id: $user_id) {
+      id
+      symbol
+      user_id
+    }
+  }
+`;
 
 export default function TabTwoScreen() {
+  const { loading, error, data } = useQuery(query, {
+    variables: { user_id: "vadim" },
+  });
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+  if (error) {
+    return <Text>Error! {error.message}</Text>;
+  }
+
+  console.log(data);
+
   return (
     <View style={styles.container}>
-      
+      <Stack.Screen options={{ title: "Favourties" }} />
+      <FlatList
+        data={[]}
+        renderItem={({ item }) => (
+          <Text>
+            <StockListItem stock={item} />
+          </Text>
+        )}
+      />
     </View>
   );
 }
@@ -13,6 +45,5 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
-  }
+  },
 });
